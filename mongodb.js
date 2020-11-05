6b607b36908ebcf51bc1cb92dbc690dc4ba563b5
Chunyang Xu <mail@xuchunyang.me>
2020-11-05 18:20:38 +0800

    * mongodb.js: Keep connection open.

--- mongodb.js
+++ mongodb.js
@@ -1,43 +1,39 @@
 const { MongoClient } = require("mongodb");
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://pc.lan:27017/epkg";
-const DB_NAME = new URL(MONGODB_URI).pathname.slice(1);
-const client = new MongoClient(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+let cacheDb = null;
+async function connectToDatabase() {
+  if (cacheDb) return cacheDb;
+
+  const MONGODB_URI = process.env.MONGODB_URI || "mongodb://pc.lan:27017/epkg";
+  const DB_NAME = new URL(MONGODB_URI).pathname.slice(1);
+  const client = new MongoClient(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log(`connecting to ${MONGODB_URI} ...`);
+  await client.connect();
+  console.log(`connected to ${MONGODB_URI}`);
+  const db = await client.db(DB_NAME);
+  cacheDb = db;
+
+  return db;
+}
 
 async function list() {
-  try {
-    console.log(`connecting to ${MONGODB_URI} ...`);
-    await client.connect();
-    console.log(`connected to ${MONGODB_URI}`);
-    const db = client.db(DB_NAME);
-    const collection = db.collection("packages");
-    const projection = { name: 1, summary: 1, _id: 0 };
-    const cursor = collection.find().project(projection);
-    const array = await cursor.toArray();
-    return array.map(({ name, summary }) => [name, summary]);
-  } finally {
-    console.log("closing mongodb connection");
-    await client.close();
-  }
+  const db = await connectToDatabase();
+  const collection = db.collection("packages");
+  const projection = { name: 1, summary: 1, _id: 0 };
+  const cursor = collection.find().project(projection);
+  const array = await cursor.toArray();
+  return array.map(({ name, summary }) => [name, summary]);
 }
 
 // list().then((x) => console.log(x));
 
 async function info(name) {
-  try {
-    console.log(`connecting to ${MONGODB_URI} ...`);
-    await client.connect();
-    console.log(`connected to ${MONGODB_URI}`);
-    const db = client.db(DB_NAME);
-    const collection = db.collection("packages");
-    return await collection.findOne({ name });
-  } finally {
-    console.log("closing mongodb connection");
-    await client.close();
-  }
+  const db = await connectToDatabase();
+  const collection = db.collection("packages");
+  return await collection.findOne({ name });
 }
 
 module.exports = { list, info };
